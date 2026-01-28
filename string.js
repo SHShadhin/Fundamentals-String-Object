67e8f74db07613128a327cafcefcff6d3c81cbce
SHShadhin <princeshadhin70@gmail.com>
2026-01-28 11:47:23 +0600

    string

---
+++ string.js
@@ -0,0 +1,25 @@
+// type of string declare rules
+
+const country = "Bangladesh";
+const division = "Rajshahi";
+const district = `Pabna`;
+const thana = new String("Bera"); // object
+
+// console.log(typeof (country))
+// console.log(typeof (division))
+// console.log(typeof (district))
+// console.log(typeof (thana))
+// console.log(thana)
+
+const numbers = [12, 34, 53, 76];
+
+numbers[0] = 222;  // that means array is mutable. // Mutable means its changable 
+console.log(numbers) 
+
+
+
+const capital = 'Dha k a'
+
+capital[0] = 'J' // string is immutable. Immutable means unchangable.
+console.log(capital)
+console.log(capital.length)
